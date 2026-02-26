f5644ffe99cc77038d711e078aee35b6c6fdd618
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-02-26 15:41:27 +0000

    change dark theme templates

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,11 +12,11 @@ const NavBar = () => {
   return (
     <div>
       <div className='py-4 px-7 flex justify-between w-full backdrop-blur-xl'>
-        <div className='cursor-pointer flex items-center'>
+        <div className='cursor-pointer flex items-center space-x-2'>
           <div className="px-4 py-2 bg-orange-500  rounded-lg">
             <h1 className='font-medium text-lg'>k</h1>
           </div>
-          <h1 className="text-xl font-light">side<span className='text-2xl bg-linear-to-r from-rose-900 to-slate-500 text-transparent bg-clip-text'>Projects</span></h1>
+          <h1 className="text-xl font-light">side<span className='text-2xl font-medium bg-linear-to-r from-blue-700 to-cyan-500 text-transparent bg-clip-text'>Projects</span></h1>
         </div>
         <ul className='text-md hidden md:flex justify-center items-center space-x-5'>
           <li className='cursor-pointer text-white border px-4 py-2 rounded-xl bg-black'>Todo App</li>
@@ -24,7 +24,7 @@ const NavBar = () => {
           <li className='cursor-pointer text-white border px-4 py-2 rounded-xl bg-black'>Weather App</li>
         </ul>
         <div className='flex items-center space-x-3'>
-          <button className='cursor-pointer flex md:hidden' onClick={() => setOpen(!open)}>{open ? (<X className='w-7 h-7' />) : (<Menu className='w-7 h-7' />)}</button>
+          <button className='cursor-pointer flex md:hidden' onClick={() => setOpen(!open)}>{open ? (<X className='w-6 h-6' />) : (<Menu className='w-6 h-6' />)}</button>
           <button className='cursor-pointer hidden md:flex' onClick={handleDark}>{dark ? (<Moon />) : (<Sun />)}</button>
         </div>
       </div>
