a3d3b719e1ccc9e1201c00b140651adf9b348d73
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-02-26 12:53:34 +0000

    initializing side projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import NavBar from './components/NavBar'
+import Todo from './pages/Todo'
+import Quiz from './pages/Quiz'
+import WeatherApp from './pages/WeatherApp'
+
 
 const App = () => {
   return (
     <div>
-      
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/todo' element={<Todo />} />
+        <Route path='/quiz' element={<Quiz />} />
+        <Route path='/weatherapp' element={<WeatherApp />} />
+      </Routes>
     </div>
   )
 }
