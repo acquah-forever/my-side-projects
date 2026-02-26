c015e901b5c05070b2c7f2b135df1c9390ebf91a
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-02-26 16:28:21 +0000

    updating todo app

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,17 +1,32 @@
 import React, { useState } from 'react'
 
 const weather = () => {
+
+    const [list, setList] = useState([])
+    const [submit, setSubmit] = useState(false)
+
+    function handleSubmit(){
+        setList([...list, submit])
+        setSubmit([])
+    }
+
     return (
+        <div>
         <div className='mt-10 p-10 max-w-2xl'>
-            <div>
-                <h1 className="font-bold text-3xl text-center">TODO LIST</h1>
+            <div className='mb-10'>
+                <h1 className="font-bold  text-4xl text-center">TODO LIST</h1>
             </div>
-            <form className='flex flex-col mb-10'>
-                <input type="text" placeholder='Enter your Todo Item' />
-            </form>
-            <button className='flex justify-start'>Add Task</button>
+                <div className='p-4 border rounded-2xl mb-10'>
+                    <input className='outline-none w-full'onChange={(e) => setSubmit(e.target.value)} type="text" placeholder='Enter your Todo Item' />
+                </div>
+            <button className='text-white flex justify-start bg-sky-500 px-7 py-3 rounded-lg cursor-pointer'onClick={handleSubmit}>Add Task</button>
+        </div>
 
-
+        <div className='bg-slate-200 max-w-4xl'>
+            <ul>
+                {list.map((e,index) => <li key={index}>{e}</li>)}
+            </ul>
+        </div>
         </div>
     )
 }
