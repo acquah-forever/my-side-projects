3e0a3b4182b28db28b9173a6717468ce9dcce492
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-02-27 19:25:28 +0000

    imported questionnaire

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -9,7 +9,7 @@ const MainMenu = () => {
   }
   return (
     <div className='mt-5 w-125 h-125 border rounded-lg bg-sky-300 flex flex-col justify-center items-center'>
-      <button className='w-75 h-12.5 m-1.25 bg-slate-400 border-none rounded p-3'onClick={handleClick}>Start Quiz</button>
+      <button className='cursor-pointer w-75 h-12.5 m-1.25 text-xl bg-slate-400 border-none rounded p-3'onClick={handleClick}>Start Quiz</button>
       
     </div>
   )
---
+++ src/helpers/QuestionBank.jsx
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+        prompt:"What is your preferred Programming Language?",
+        optionA:"Rust",
+        optionB:"php",
+        optionC:"JavaScript",
+        optionD:"Java",
+        answer:"JavaScript"
+    },
+    {
+        prompt:"What is your preferred Framework?",
+        optionA:"Vue.js",
+        optionB:"React.js",
+        optionC:"Angular",
+        optionD:"Node.js",
+        answer:"React.js"
+    },
+    {
+        prompt:"Which state is the most used state in React?",
+        optionA:"useQuerry",
+        optionB:"useContext",
+        optionC:"useReducer",
+        optionD:"useState",
+        answer:"useState"
+    }
+]
