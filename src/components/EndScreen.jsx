49cda013548a54a1e4d8abaa62d6129842763c2e
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-02-27 22:44:29 +0000

    added Quiz logic to make app functional

--- src/components/EndScreen.jsx
+++ src/components/EndScreen.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const EndScreen = () => {
   return (
     <div>
-      <h1>End Screen</h1>
+
     </div>
   )
 }
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -3,35 +3,40 @@ import { QuizContext } from '../helpers/Context'
 import { QuestionBank } from '../helpers/QuestionBank'
 
 const Questions = () => {
-  const {score, setScore} = useContext(QuizContext)
+  const { score, setScore, setGameState } = useContext(QuizContext)
   const [currentQuestion, setCurrentQuestion] = useState(0)
   const [optionChosen, setOptionChosen] = useState('')
 
-  function nextQuestion(){
-    if(QuestionBank[currentQuestion].answer == optionChosen){
+  function nextQuestion() {
+    if (QuestionBank[currentQuestion].answer == optionChosen) {
       setScore(score + 1)
     }
-    setCurrentQuestion()
+    setCurrentQuestion(currentQuestion + 1)
+  }
+
+  function finishquiz() {
+    if (QuestionBank[currentQuestion].answer == optionChosen) {
+      setScore(score + 1)
+    }
+
+    setGameState('endscreen')
+
   }
 
   return (
     <div>
       <h1>{QuestionBank[currentQuestion].prompt}</h1>
       <div className='space-x-2 mb-3 mt-3'>
-        <button className='cursor-pointer bg-gray-400 p-1 rounded' onClick={() => setOptionChosen('A')}>
-          {QuestionBank[currentQuestion].optionA}
-        </button>
-        <button className='cursor-pointer bg-gray-400 p-1 rounded' onClick={() => setOptionChosen('B')}>
-          {QuestionBank[currentQuestion].optionB}
-        </button>
-        <button className='cursor-pointer bg-gray-400 p-1 rounded' onClick={() => setOptionChosen('C')}>
-          {QuestionBank[currentQuestion].optionC}
-        </button>
-        <button className='cursor-pointer bg-gray-400 p-1 rounded' onClick={() => setOptionChosen('D')}>
-          {QuestionBank[currentQuestion].optionD}
-        </button>
+        <button className='cursor-pointer bg-gray-400 p-1 rounded' onClick={() => setOptionChosen('A')}>{QuestionBank[currentQuestion].optionA}</button>
+        <button className='cursor-pointer bg-gray-400 p-1 rounded' onClick={() => setOptionChosen('B')}>{QuestionBank[currentQuestion].optionB}</button>
+        <button className='cursor-pointer bg-gray-400 p-1 rounded' onClick={() => setOptionChosen('C')}>{QuestionBank[currentQuestion].optionC}</button>
+        <button className='cursor-pointer bg-gray-400 p-1 rounded' onClick={() => setOptionChosen('D')}>{QuestionBank[currentQuestion].optionD}</button>
       </div>
-      <button className='cursor-pointer bg-gray-400 p-1 rounded'>Next Question</button>
+      {currentQuestion == QuestionBank.length - 1 ? (
+        <button className='cursor-pointer bg-gray-400 p-1 rounded' onClick={finishquiz}>Finish quiz</button>
+      ) : (
+        <button className='cursor-pointer bg-gray-400 p-1 rounded' onClick={nextQuestion}>Next Question</button>
+      )}
     </div>
   )
 }
