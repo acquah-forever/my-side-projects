d3e2d8258bbd06fed58c0fda63a4fa4dddcefd69
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-02-27 18:10:51 +0000

    initiated Quiz.jsx

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,22 +1,11 @@
 import React from 'react'
-import { quiz } from '../../constants/Constant'
+
 
 const Quiz = () => {
   return (
     <div>
-      <ul>
-        {questions.map((e,index) => <li key={index}>
-          <h1>{e.question}</h1>
-          <ul>
-            {e.answers.map((ans,i) => <li key={i}>
-              {ans}
-            </li>)}
-          </ul>
-        </li>)}
 
-      
-      </ul> 
-      
+
     </div>
   )
 }
