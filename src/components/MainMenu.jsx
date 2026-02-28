966a913e785b26eadc804dfd847e980cdd0ef3af
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-02-28 22:03:05 +0000

    formated ToDo list

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -9,7 +9,7 @@ const MainMenu = () => {
   }
   return (
     <div className='mt-5 w-125 h-125 border rounded-lg bg-sky-500 flex flex-col justify-center items-center'>
-      <button className='cursor-pointer w-75 h-auto m-1.25 text-2xl font-medium bg-slate-400 border-2 rounded-lg p-5'onClick={handleClick}>Start Quiz</button>
+      <button className='cursor-pointer text-black dark:text-white w-75 h-auto m-1.25 text-2xl font-medium bg-slate-400 border-2 rounded-lg p-5'onClick={handleClick}>Start Quiz</button>
       
     </div>
   )
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -25,9 +25,9 @@ const ToDoList = () => {
         <div>
             <div className='shadow-[8px_8px_8px_8px_black] dark:shadow-none dark:border-none p-15 rounded-xl'>
                 <div className='flex flex-col mb-10'>
-                    <h1 className="text-center font-semibold text-3xl mb-7">CLASSY TODO LIST 📋 😊</h1>
+                    <h1 className="text-center text-black dark:text-white font-semibold text-3xl mb-7">CLASSY TODO LIST 📋 😊</h1>
                     <div className='w-full'>
-                        <input className={`w-full p-4 outline-none border-2 rounded-lg ${isClicked ? 'border-green-500' : 'border-blue-400'} `} onChange={handleChange} value={newTask} type="text" placeholder='Enter Task' />
+                        <input className={`w-full text-black dark:text-white p-4 outline-none border-2 rounded-lg ${isClicked ? 'border-green-500' : 'border-blue-400'} `} onChange={handleChange} value={newTask} type="text" placeholder='Enter Task' />
                     </div>
                 </div>
                 <button className='cursor-pointer mb-7 p-3 bg-blue-300 border rounded-lg disabled:opacity-50' onClick={handleClick} disabled={!newTask.trim()}>Add Task</button>
