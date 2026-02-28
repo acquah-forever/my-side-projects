ecdff246d29f6d3125fdb01681b3597507798bcb
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-02-28 22:44:07 +0000

    configured inputarea.

--- src/pages/WeatherApp.jsx
+++ src/pages/WeatherApp.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
-import { Search,Sun } from 'lucide-react'
+import React, { useState } from 'react'
+import { Sun } from 'lucide-react'
 
 
 const WeatherApp = () => {
+  const [text, setText] = useState('')
+  const [newText, setNewText] = useState('')
+  function handleChange(event) {
+    if (!text.trim()) return
+    setNewText(event.target.value)
+  }
+  function handleAdd(){
+    setText(newText)
+  }
   return (
     <div className='flex justify-center items-center'>
-    <div className='max-w-70 h-110 p-12 rounded-2xl flex flex-col justify-center items-center bg-red-300'>
-      <div className='mb-12 flex items-center space-x-4'>
-       <input className='border p-2 rounded-2xl' type="text" placeholder='Enter City' />
-       <Search />
+      <div className='text-black dark:text-white max-w-90 h-130 p-12 rounded-2xl flex flex-col justify-center items-center bg-red-300'>
+        <div className='mb-12 flex items-center space-x-6'>
+          <input className='border p-2 rounded-2xl' onChange={handleChange} value={text} type="text" placeholder='Enter City' />
+          <button className='cursor-pointer border p-2 bg-sky-400 rounded'>Search</button>
+          <h1>{newText}</h1>
+        </div>
+        <Sun className='w-15 h-15 mb-10' />
+        <h1 className='mb-12 text-5xl'>41C</h1>
+        <h1 className='mb-7 text-xl'>Newport</h1>
+        <div className='flex space-x-5'>
+          <h1 className='text-xl'>Humidity</h1>
+          <h1 className='text-xl'>Wind Speed</h1>
+        </div>
       </div>
-       <Sun className='mb-10' />
-       <h1 className='mb-12'>41C</h1>
-       <h1 className='mb-7'>Newport</h1>
-       <div className='flex space-x-5'>
-       <h1>Humidity</h1>
-       <h1>Wind Speed</h1>
-       </div>
-    </div>
     </div>
   )
 }
