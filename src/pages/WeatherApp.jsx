2c0c790b0d6972d22eb7203ea02bf21b7bedf0d7
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-02-28 22:18:51 +0000

    designing WeatherApp

--- src/pages/WeatherApp.jsx
+++ src/pages/WeatherApp.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Search,Sun } from 'lucide-react'
+
 
 const WeatherApp = () => {
   return (
-    <div>
-       
+    <div className='max-w-80 h-100 rounded-2xl flex flex-col justify-center items-center bg-red-300'>
+      <div className='flex items-center space-x-4'>
+       <input className='border p-2 rounded-2xl' type="text" placeholder='Enter City' />
+       <Search />
+      </div>
+       <Sun />
+       <h1>41C</h1>
+       <h1>Newport</h1>
+       <h1>Humidity</h1>
+       <h1>Wind Speed</h1>
     </div>
   )
 }
