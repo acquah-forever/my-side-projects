52cd0672a2d1813e7e4fce180f7a325f5ab5effb
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-02-28 23:01:59 +0000

    debugging WeatherApp

--- src/pages/WeatherApp.jsx
+++ src/pages/WeatherApp.jsx
@@ -3,30 +3,30 @@ import { Sun } from 'lucide-react'
 
 
 const WeatherApp = () => {
-  const [text, setText] = useState('')
+  const [text, setText] = useState([])
   const [newText, setNewText] = useState('')
   function handleChange(event) {
-    if (!text.trim()) return
     setNewText(event.target.value)
   }
-  function handleAdd(){
+  function handleNewText(){
     setText(newText)
   }
+
   return (
-    <div className='flex justify-center items-center'>
-      <div className='text-black dark:text-white max-w-90 h-130 p-12 rounded-2xl flex flex-col justify-center items-center bg-red-300'>
-        <div className='mb-12 flex items-center space-x-6'>
-          <input className='border p-2 rounded-2xl' onChange={handleChange} value={text} type="text" placeholder='Enter City' />
-          <button className='cursor-pointer border p-2 bg-sky-400 rounded'>Search</button>
-          <h1>{newText}</h1>
-        </div>
-        <Sun className='w-15 h-15 mb-10' />
-        <h1 className='mb-12 text-5xl'>41C</h1>
-        <h1 className='mb-7 text-xl'>Newport</h1>
-        <div className='flex space-x-5'>
-          <h1 className='text-xl'>Humidity</h1>
-          <h1 className='text-xl'>Wind Speed</h1>
-        </div>
+    <div className='mx-auto text-black dark:text-white max-w-90 h-130 p-12 rounded-2xl flex flex-col justify-center items-center bg-red-300'>
+      <div className='mb-10 flex items-center space-x-6'>
+        <input className='border p-2 outline-none rounded-2xl' onChange={handleChange} placeholder='Enter City' />
+        <button className='cursor-pointer border p-2 bg-sky-400 rounded'onClick={handleNewText}>Search</button>
+      </div>
+      <ul>
+        {text.map((e,index) => <li key={index}>{e}</li>)}
+      </ul>
+      <Sun className='w-15 h-15 mb-10' />
+      <h1 className='mb-12 text-5xl'>41C</h1>
+      <h1 className='mb-7 text-xl'>Newport</h1>
+      <div className='flex space-x-5'>
+        <h1 className='text-xl'>Humidity</h1>
+        <h1 className='text-xl'>Wind Speed</h1>
       </div>
     </div>
   )
