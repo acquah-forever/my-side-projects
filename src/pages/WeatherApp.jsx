4658f910a5ef6d0ebfdb6f78cef74006091e5968
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-02-28 23:22:45 +0000

    still debugging WeatherApp

--- src/pages/WeatherApp.jsx
+++ src/pages/WeatherApp.jsx
@@ -5,21 +5,31 @@ import { Sun } from 'lucide-react'
 const WeatherApp = () => {
   const [text, setText] = useState([])
   const [newText, setNewText] = useState('')
+  const [greenBorder, setGreenBorder] = useState(false)
   function handleChange(event) {
     setNewText(event.target.value)
   }
   function handleNewText(){
-    setText(newText)
+    if(!newText.trim()) return
+    setText((prevText) => [...prevText,newText])
+    setNewText('')
+    setGreenBorder(true)
+    setTimeout(() => {
+      setGreenBorder(false)
+    },2000)
+  }
+  function removeText(index){
+    setText(text.filter((_,i) => i !== index))
   }
 
   return (
-    <div className='mx-auto text-black dark:text-white max-w-90 h-130 p-12 rounded-2xl flex flex-col justify-center items-center bg-red-300'>
+    <div className='mx-auto border-2 text-black dark:text-white max-w-90 h-130 p-12 rounded-2xl flex flex-col justify-center items-center'>
       <div className='mb-10 flex items-center space-x-6'>
-        <input className='border p-2 outline-none rounded-2xl' onChange={handleChange} placeholder='Enter City' />
+        <input className={`border-2 p-2 outline-none ${greenBorder ? 'border-green-500' : 'border-black'} rounded-2xl`} onChange={handleChange} value={newText} placeholder='Enter City' />
         <button className='cursor-pointer border p-2 bg-sky-400 rounded'onClick={handleNewText}>Search</button>
       </div>
       <ul>
-        {text.map((e,index) => <li key={index}>{e}</li>)}
+        {text.map((e,index) => <li key={index}onClick={() => removeText(index)}>{e}</li>)}
       </ul>
       <Sun className='w-15 h-15 mb-10' />
       <h1 className='mb-12 text-5xl'>41C</h1>
