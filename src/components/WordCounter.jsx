dbba948aef5866f191066576357c8c963b91f5ce
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-03-01 17:41:41 +0000

    edited WordCounter.jsx

--- src/components/WordCounter.jsx
+++ src/components/WordCounter.jsx
@@ -1,13 +1,37 @@
 import React from 'react'
 
 const WordCounter = () => {
-  return (
-    <div>
-        <hi className="font-medium text-3xl">WORD COUNTER</hi>
-        <input type="text" placeholder='Enter You Text' />
-      
-    </div>
-  )
+    return (
+        <div className='border'>
+            <div className='flex justify-center mb-10'>
+                <hi className="font-medium text-3xl">WORD COUNTER</hi>
+            </div>
+            <div className='flex justify-center border'>
+                <div>
+                    <input type="text" placeholder='Enter You Text' />
+                </div>
+
+                <div className='grid grid-cols-2'>
+                    <div>
+                        <span className="font-medium text-2xl">0</span>
+                        <h1 className="text-2xl font-medium">WORDS</h1>
+                    </div>
+                    <div>
+                        <span className="font-medium text-2xl">0</span>
+                        <h1 className="font-medium text-2xl">FACEBOOK</h1>
+                    </div>
+                    <div>
+                        <span className="font-medium text-2xl">0</span>
+                        <h1 className="font-medium text-2xl">TWITTER</h1>
+                    </div>
+                    <div>
+                        <span className="font-medium text-2xl">0</span>
+                        <hi className="font-medium text-2xl">INSTAGRAM</hi>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default WordCounter
