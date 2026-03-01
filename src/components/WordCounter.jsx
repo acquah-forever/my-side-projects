8524cc7464bbbb61ec97eddc819084dece1fc6af
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-03-01 19:37:00 +0000

    included conditional rendering in WordCount

--- src/components/WordCounter.jsx
+++ src/components/WordCounter.jsx
@@ -2,16 +2,30 @@ import React,{useState} from 'react'
 
 const WordCounter = () => {
 
+
+    const [type, setType] = useState('')
     const [count, setCount] = useState(0)
+    const [face, setFace] = useState(20)
+    const[twitter, setTwitter] = useState(20)
     const [word, setWord] = useState(0)
-    const [face, setFace] = useState(150)
-    const[twitter, setTwitter] = useState(300)
 
     function handleChange(event){
-        setCount(event.target.value)
+        const value = event.target.value
+        setType(value)
 
+        const digits = value.length
+
+        setCount(digits)
+        setFace(10 - digits)
+        setTwitter(20 - digits)
+
+        const words = value.trim() === "" ? 
+        (0) : (value.trim().split(/\s+/).length)
+
+        setWord(words)
     }
-    
+
+
     return (
         <div className='border mx-auto max-w-4xl h-150'>
             <div className='bg-sky-400 p-10 w-full flex justify-center mb-10'>
@@ -19,24 +33,24 @@ const WordCounter = () => {
             </div>
             <div className='grid grid-cols-1 md:flex md:justify-between px-7 h-80'>
                 <div className='mt-12'>
-                    <input className='outline-none  py-2  w-5/2'onChange={handleChange} type="text" placeholder='Enter You Text' />
+                    <input className='outline-none py-2  w-4/2'onChange={handleChange}  placeholder='Enter You Text' />
                 </div>
 
                 <div className='px-5 text-center gap-x-20 gap-y-20 grid grid-cols-2 items-center'>
                     <div>
                         <span className="font-medium text-2xl">{count}</span>
-                        <h1 className="text-2xl font-medium">ALPHABETS</h1>
+                        <h1 className="text-2xl font-medium">LETTERS</h1>
                     </div>
                     <div>
-                        <span className="font-medium text-2xl">{face}</span>
-                        <h1 className="font-medium text-2xl">FACEBOOK</h1>
+                        <span className={`font-medium text-2xl ${face < 0 ? "text-red-500" : "text-black dark:text-white"}`}>{face}</span>
+                        <h1 className={`font-medium text-2xl ${face < 0 ? "text-red-500" : "text-black dark:text-white"}`}>FACEBOOK</h1>
                     </div>
                     <div>
-                        <span className="font-medium text-2xl">{twitter}</span>
-                        <h1 className="font-medium text-2xl">TWITTER</h1>
+                        <span className={`font-medium text-2xl ${twitter < 0 ? "text-red-500" : "text-black dark:text-white"}`}>{twitter}</span>
+                        <h1 className={`font-medium text-2xl ${twitter < 0 ? "text-red-500" : "text-black dark:text-white"}`}>TWITTER</h1>
                     </div>
                     <div>
-                        <span className="font-medium text-2xl">0</span>
+                        <span className="font-medium text-2xl">{word}</span>
                         <h1 className="font-medium text-2xl">WORDS</h1>
                     </div>
                 </div>
