538c413b524d770f687a7f076f31e52f2600a07b
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-03-02 19:48:02 +0000

    added comment counter

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -9,6 +9,8 @@ const Comments = () => {
   const [newText, setNewText] = useState('')
   const [wordCount, setWordCount] = useState(digits)
   const [isActive, setIsActive] = useState(false)
+  const [count, setCount] = useState(0)
+  const [commentCount, setCommentCount] = useState(0)
 
   function handleChange(e) {
     const value = e.target.value
@@ -41,9 +43,14 @@ const Comments = () => {
       setIsActive(false)
     }, 1000)
 
+    setCommentCount(commentCount + 1)
+
   }
   
-  function handleDelete(){
+  function handleDelete(index){
+    setText(text.filter((_,i) => i !== index))
+    setCommentCount(commentCount - 1)
+
 
   }
 
@@ -53,24 +60,27 @@ const Comments = () => {
       <div className='flex flex-col'>
         <div className={`border-2 rounded-xl p-1 mb-5 ${isActive ? "border-green-500" : "border-slate-300"}`}>
           <input className='outline-none w-full px-1 py-2' onChange={handleChange} type="text" value={newText} placeholder='Enter comment' />
-          <span className='flex justify-end'>{wordCount}</span>
+          <span className='flex justify-end'>Number of Words :{wordCount}</span>
         </div>
         <div className='flex justify-end'>
           <button className={`cursor-pointer ${isActive ? "bg-green-500" : "bg-sky-400"} p-3 w-40  rounded-lg`} onClick={handleClick}>Send Comment</button>
         </div>
       </div>
-      <h1>Total Comments: <span>0</span></h1>
+      <h1>Total Comments: <span>{commentCount}</span></h1>
       <div className='flex flex-col border mt-7 h-80 px-3 py-5 w-full overflow-y-scroll text-black'>
         <ul>
           {text.map((e, index) =>
             <li className='bg-sky-300 dark:bg-sky-200 rounded-lg w-full mb-3 px-3 py-1' key={index}>{e}
             <div className='flex items-center space-x-2 mt-1'>
               <ThumbsUp className='w-5 h-5' />
+              <span>0</span>
               <ThumbsDown className='w-5 h-5' />
+              <span>0</span>
               <Heart className='w-5 h-5' />
+              <span>0</span>
             </div>
             <div className='flex justify-end'>
-            <button className='px-4 py-1 border rounded-lg bg-indigo-400 'onClick={() => handleDelete(index)}>Delete</button>
+            <button className='cursor-pointer px-4 py-1 border rounded-lg bg-indigo-400 'onClick={() => handleDelete(index)}>Delete</button>
             </div>
             </li>)}
         </ul>
