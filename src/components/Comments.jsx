c0b7fa16405d0b41b18c8ebae44e74ea3db0c21d
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-03-02 13:46:54 +0000

    added "send comment" button

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 
 const Comments = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-medium">INTERACTIVE COMMENT SECTION</h1>
-      <input type="text" placeholder='Enter comment' />
-      <span>200</span>
-      <button className='bg-sky-400 p-2 rounded-lg'>Send Comment</button>
+    <div className='p-10 border max-w-3xl h-150'>
+      <h1 className="text-3xl text-center font-medium mb-7">INTERACTIVE COMMENT SECTION</h1>
+      <div flex flex-col>
+        <div className='border border-blue-300 rounded-xl p-2 mb-5'>
+          <input className='outline-none w-full' type="text" placeholder='Enter comment' />
+          <span className='flex justify-end'>200</span>
+        </div>
+        <div>
+        </div>
+        <div className='flex justify-end'>
+          <button className='bg-sky-400 p-3 w-40  rounded-lg'>Send Comment</button>
+        </div>
+      </div>
     </div>
   )
 }
