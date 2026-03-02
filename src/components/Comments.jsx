64c11035c609800cb586a0d775c9eeedea881497
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-03-02 16:01:58 +0000

    finalised conditional rendering

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 const digits = 200
 
@@ -7,31 +7,30 @@ const Comments = () => {
   const [newText, setNewText] = useState('')
   const [wordCount, setWordcount] = useState(digits)
 
-  function handleChange(e){
+  function handleChange(e) {
     const value = e.target.value
-    const amountOfWords = value.trim().split(/\s+/).length
     setNewText(value)
-    setWordcount(digits - amountOfWords)
-    
+    const words = value.trim() === "" ? (0) : (value.trim().split(/\s+/).length)
+    setWordcount(digits - words)
   }
 
-  function handleClick(){
-    !newText.trim() ? 
-    alert("Please Fill Input Correctly") : 
-    (setText((prevText) => [...prevText, newText]))
+  function handleClick() {
+    !newText.trim() ?
+      alert("Please Fill Input Correctly") :
+      (setText((prevText) => [...prevText, newText]))
     setNewText('')
-    }
+  }
 
   return (
     <div className='p-10 border max-w-3xl h-150'>
       <h1 className="text-3xl text-center font-medium mb-7">INTERACTIVE COMMENT SECTION</h1>
       <div flex flex-col>
         <div className='border border-blue-300 rounded-xl p-1 mb-5'>
-          <input className='outline-none w-full px-1 py-2'onChange={handleChange} type="text" value={newText} placeholder='Enter comment' />
+          <input className='outline-none w-full px-1 py-2' onChange={handleChange} type="text" value={newText} placeholder='Enter comment' />
           <span className='flex justify-end'>{wordCount}</span>
         </div>
         <div className='flex justify-end'>
-          <button className='cursor-pointer bg-sky-400 p-3 w-40  rounded-lg'onClick={handleClick}>Send Comment</button>
+          <button className='cursor-pointer bg-sky-400 p-3 w-40  rounded-lg' onClick={handleClick}>Send Comment</button>
         </div>
       </div>
       <p>{text}</p>
