7155295170a6d75584eb01e020be1275789930af
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-03-02 15:21:12 +0000

    added conditional logic statements

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,12 +1,22 @@
 import React,{useState} from 'react'
 
+const digits = 200
+
 const Comments = () => {
   const [text, setText] = useState([])
   const [newText, setNewText] = useState('')
+  const [wordCount, setWordcount] = useState(digits)
 
   function handleChange(e){
     const value = e.target.value
+    const amountOfWords = value.trim().split(/\s+/).length
     setNewText(value)
+    setWordcount(digits - amountOfWords)
+    if(newText.trim() == ""){
+      0
+    } else {
+      wordCount
+    }
   }
 
   function handleClick(){
@@ -20,14 +30,15 @@ const Comments = () => {
     <div className='p-10 border max-w-3xl h-150'>
       <h1 className="text-3xl text-center font-medium mb-7">INTERACTIVE COMMENT SECTION</h1>
       <div flex flex-col>
-        <div className='border border-blue-300 rounded-xl p-2 mb-5'>
-          <input className='outline-none w-full'onChange={handleChange} type="text" value={newText} placeholder='Enter comment' />
-          <span className='flex justify-end'>200</span>
+        <div className='border border-blue-300 rounded-xl p-1 mb-5'>
+          <input className='outline-none w-full px-1 py-2'onChange={handleChange} type="text" value={newText} placeholder='Enter comment' />
+          <span className='flex justify-end'>{wordCount}</span>
         </div>
         <div className='flex justify-end'>
           <button className='cursor-pointer bg-sky-400 p-3 w-40  rounded-lg'onClick={handleClick}>Send Comment</button>
         </div>
       </div>
+      <p>{text}</p>
     </div>
   )
 }
