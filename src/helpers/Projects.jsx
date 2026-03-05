fe484f2f27bd46e13187e657d7a4e963fdc5981f
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-03-05 22:14:14 +0000

    added ids

--- src/helpers/Projects.jsx
+++ src/helpers/Projects.jsx
@@ -1,18 +1,24 @@
 export const Projects = [
     {
+        id:1,
         title: "ToDo App",
         description: "A productivity web application that allows users to create, manage, and track daily tasks. It helps users stay organized by adding, completing, and deleting tasks in an intuitive interface"
 
     },
     {
+        id:2,
         title: "Weather App",
         description: "A productivity web application that allows users to create, manage, and track daily tasks. It helps users stay organized by adding, completing, and deleting tasks in an intuitive interface"
 
-    }, {
+    }, 
+    {
+        id:3,
         title: "Quiz App",
         description: "A productivity web application that allows users to create, manage, and track daily tasks. It helps users stay organized by adding, completing, and deleting tasks in an intuitive interface"
 
-    }, {
+    }, 
+    {
+        id:4,
         title: "Comment Section",
         description: "A productivity web application that allows users to create, manage, and track daily tasks. It helps users stay organized by adding, completing, and deleting tasks in an intuitive interface"
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import CurrentProjects from '../components/CurrentProjects'
+import Projects from './helper/Projects'
 
 
 const Home = () => {
@@ -7,6 +9,12 @@ const Home = () => {
             <h1 className="font-semibold text-3xl">My Practice Projects</h1>
             <h2 className="font-medium">A shoecase of the innovative tools, systems, and experiments I am currently building.</h2>
             <button>View Projects</button>
+            <h1>Current Projects</h1>
+            <div>
+                {data.map((projects,id))}
+
+            </div>
+
 
         </div>
     )
