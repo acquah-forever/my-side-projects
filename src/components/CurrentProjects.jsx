bf2093968a47553646ebe1afb16558d6356b9d51
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-03-05 22:09:33 +0000

    created a map function

--- src/components/CurrentProjects.jsx
+++ src/components/CurrentProjects.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
-const CurrentProjects = () => {
+const CurrentProjects = (props) => {
   return (
     <div>
-       
-
-      
+        <h1>{props.title}</h1>
+        <p>{props.description}</p>
     </div>
   )
 }
---
+++ src/helpers/Projects.jsx
@@ -0,0 +1,20 @@
+export const Projects = [
+    {
+        title: "ToDo App",
+        description: "A productivity web application that allows users to create, manage, and track daily tasks. It helps users stay organized by adding, completing, and deleting tasks in an intuitive interface"
+
+    },
+    {
+        title: "Weather App",
+        description: "A productivity web application that allows users to create, manage, and track daily tasks. It helps users stay organized by adding, completing, and deleting tasks in an intuitive interface"
+
+    }, {
+        title: "Quiz App",
+        description: "A productivity web application that allows users to create, manage, and track daily tasks. It helps users stay organized by adding, completing, and deleting tasks in an intuitive interface"
+
+    }, {
+        title: "Comment Section",
+        description: "A productivity web application that allows users to create, manage, and track daily tasks. It helps users stay organized by adding, completing, and deleting tasks in an intuitive interface"
+
+    }
+]
