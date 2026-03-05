f6a0a4dc9f6fee48b0032be0eab25509f30ebea8
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-03-05 22:05:32 +0000

    created a new component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
         <div>
             <h1 className="font-semibold text-3xl">My Practice Projects</h1>
             <h2 className="font-medium">A shoecase of the innovative tools, systems, and experiments I am currently building.</h2>
+            <button>View Projects</button>
 
         </div>
     )
