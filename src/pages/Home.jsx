c8bdc9b6424e075f8801e8ba915c4c093f78859a
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-03-05 22:01:04 +0000

    initiated Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 
-
 const Home = () => {
     return (
         <div>
+            <h1 className="font-semibold text-3xl">My Practice Projects</h1>
+            <h2 className="font-medium">A shoecase of the innovative tools, systems, and experiments I am currently building.</h2>
 
         </div>
     )
