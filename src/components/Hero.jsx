1f262397ed3857585c68db3bd68cd1505ae4dde1
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-03-06 11:35:16 +0000

    added dark-overlay to bg image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,13 +3,14 @@ import Tech from "../assets/tech.jpg";
 
 const Hero = () => {
   return (
-    <div className="relative min-h-screen flex flex-col justify-center items-center">
-      <div className="absolute z-0 object-center">
-        <img className="w-full h-full" src={Tech} alt="image" />
+    <div className="flex flex-col justify-center items-center">
+      <div className="relative w-full h-180">
+        <div className="absolute z-10 bg-black/30 inset-0"></div>
+        <img className="w-full h-full object-cover object-center" src={Tech} alt="image" />
       </div>
-      <div className='flex flex-col justify-center items-center'>
-        <h1 className="font-semibold text-3xl text-center">My Practice Projects</h1>
-        <h2 className="font-medium">
+      <div className='text-white p-5 space-y-7 absolute z-20 flex flex-col justify-center items-center'>
+        <h1 className="font-semibold text-5xl text-center">My Practice Projects</h1>
+        <h2 className="font-medium text-2xl">
           A showcase of the innovative tools, systems, and experiments I am currently building.
         </h2>
         <button className='cursor-pointer bg-sky-400 p-3 rounded'>View Projects</button>
