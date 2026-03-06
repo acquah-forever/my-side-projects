b560ba4d6f0446fd9a9a6430943be78e25af7ee6
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-03-06 10:44:24 +0000

    updated background image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import Tech from '../assets/techimage.jpg'
+import React from "react";
+import Tech from "../assets/tech.jpg";
 
 const Hero = () => {
-    return (
-        <section>
-            <div>
-            <img src={Tech} alt='image' />
-            </div>
-            <h1 className="font-semibold text-3xl">My Practice Projects</h1>
-            <h2 className="font-medium">A showcase of the innovative tools, systems, and experiments I am currently building.</h2>
-            <button>View Projects</button>
-            <h1>Current Projects</h1>
+  return (
+    <div>
+        <img src={Tech} alt="image" />
+      <h1 className="font-semibold text-3xl">My Practice Projects</h1>
+      <h2 className="font-medium">
+        A showcase of the innovative tools, systems, and experiments I am currently building.
+      </h2>
 
-        </section>
-    )
-}
+      <button>View Projects</button>
+      <h1>Current Projects</h1>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
