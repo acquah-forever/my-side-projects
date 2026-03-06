525a0d9cf9e3d07765dc73480c907ca6da13a06f
Kojo Acquah Addo <kojoaddo598@gmail.com>
2026-03-06 11:08:31 +0000

    added positioning to bg image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,18 @@ import Tech from "../assets/tech.jpg";
 
 const Hero = () => {
   return (
-    <div>
-        <img src={Tech} alt="image" />
-      <h1 className="font-semibold text-3xl">My Practice Projects</h1>
-      <h2 className="font-medium">
-        A showcase of the innovative tools, systems, and experiments I am currently building.
-      </h2>
+    <div className="relative min-h-screen flex flex-col justify-center items-center">
+      <div className="absolute z-0 object-center">
+        <img className="w-full h-full" src={Tech} alt="image" />
+      </div>
+      <div className='flex flex-col justify-center items-center'>
+        <h1 className="font-semibold text-3xl text-center">My Practice Projects</h1>
+        <h2 className="font-medium">
+          A showcase of the innovative tools, systems, and experiments I am currently building.
+        </h2>
+        <button className='cursor-pointer bg-sky-400 p-3 rounded'>View Projects</button>
+      </div>
 
-      <button>View Projects</button>
-      <h1>Current Projects</h1>
     </div>
   );
 };
